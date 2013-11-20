c35b8bd936f49742a909a937a25735473c5c7d5e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-11-20 15:13:48 +0100

    new salt compat

--- _scripts/reload_grains.sh
+++ _scripts/reload_grains.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+{% if __salt__ is defined %}
 {% set ret=__salt__['saltutil.sync_grains']() %}
+{% else %}
+{% set ret=salt['saltutil.sync_grains']() %}
+{% endif %}
 echo "{{ret}}"
