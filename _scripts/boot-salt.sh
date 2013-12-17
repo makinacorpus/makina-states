e55021878c213cc011406561b03cca16323d2cb1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-12-17 03:57:04 +0100

    salt side

--- _scripts/boot-salt.sh
+++ _scripts/boot-salt.sh
@@ -1123,6 +1123,10 @@ EOF
             cat > $CONF_PREFIX/minion << EOF
 file_roots: {"base":["$ROOT"]}
 pillar_roots: {"base":["$PILLAR"]}
+module_dirs: [$ROOT/_modules, $MS/makina-states/_modules]
+returner_dirs: [$ROOT/_returners, $MS/makina-states/_returners]
+states_dirs: [$ROOT/_statess, $MS/makina-states/_states]
+render_dirs: [$ROOT/_renderers, $MS/makina-states/_renderers]
 EOF
         fi
         # run salt master+minion boot_env bootstrap
@@ -1498,6 +1502,10 @@ EOF
             cat > $MCONF_PREFIX/minion << EOF
 file_roots: {"base":["$MASTERSALT_ROOT"]}
 pillar_roots: {"base":["$MASTERSALT_PILLAR"]}
+module_dirs: [$MASTERSALT_ROOT/_modules, $MASTERSALT_MS/makina-states/_modules]
+returner_dirs: [$MASTERSALT_ROOT/_returners, $MASTERSALT_MS/makina-states/_returners]
+states_dirs: [$MASTERSALT_ROOT/_statess, $MASTERSALT_MS/makina-states/_states]
+render_dirs: [$MASTERSALT_ROOT/_renderers, $MASTERSALT_MS/makina-states/_renderers]
 EOF
         fi
 
