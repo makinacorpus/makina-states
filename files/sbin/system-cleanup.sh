3b77eab31dee69f0fdfd35fe47da16b463368125
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-01-06 19:28:14 +0100

    bootstrap msge

--- files/sbin/system-cleanup.sh
+++ files/sbin/system-cleanup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+YELLOW='\e[1;33m'
+RED="\\033[31m"
+CYAN="\\033[36m"
+NORMAL="\\033[0m"
+DEBUG=${BOOT_SALT_DEBUG:-}
+output() { echo -e "${YELLOW}$@${NORMAL}" >&2; }
 output " [*] Cleaning vm to reduce disk space usage"
 output " [*] Cleaning apt"
 apt-get clean -y
