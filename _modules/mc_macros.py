e983d5cb1ca87872cd215c806bce76fc1b205d53
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-01-12 13:50:57 +0000

    handle the case of the vagrant user

--- _modules/mc_macros.py
+++ _modules/mc_macros.py
@@ -15,6 +15,11 @@ from salt.exceptions import SaltException
 _default_activation_status = object()
 
 
+def is_item_active(config_entry, default_status=False):
+    return __salt__['mc_utils.get'](
+        config_entry, default_status)
+
+
 def get_registries(registries_configurations):
     """
     Mangle a registry of activated/unactived states to be run
@@ -69,16 +74,18 @@ def get_registries(registries_configurations):
         for item, data in registry['defaults'].items():
             activation_status = _default_activation_status
             if isinstance(data, dict):
-                activation_status = __salt__['mc_utils.get'](
+                activation_status = is_item_active(
                     registry['grains_pref'] + item,
                     data.get('active', activation_status))
-            if activation_status:
-                registry['actives'][item] = data
-            elif activation_status is not _default_activation_status:
-                registry['unactivated'][item] = data
-            else:
-                registry['availables'][item] = data
+                if activation_status is not _default_activation_status:
+                    if activation_status:
+                        registry['actives'][item] = data
+                    else:
+                        registry['unactivated'][item] = data
+                else:
+                    registry['availables'][item] = data
     return registries
+
 
 def is_active(registry, kind, name):
     try:
