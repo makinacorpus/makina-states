3c966e48ac04bb579c19352c2b12227b96bc15f0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-01-30 17:07:57 +0100

    pythonpath helper (to generate tagsfiles for eg: vim)

---
+++ _scripts/gentags_env.py
@@ -0,0 +1,23 @@
+#!bin/mypy
+# generate python tags to be imported from inside and  outside the VM (from the hosts)
+# main utility is to generate vim tags afterwards importing the .env in the shell
+import os, sys
+CWD = os.path.dirname(os.path.abspath('$(dirname $0)'))
+open('tags.env', 'w').write((
+"""#!/usr/bin/env bash
+CWD="$PWD"
+PRE=''
+#echo $CWD
+if [ x"${{PWD%%*VM*}}" = "x" ];then
+    PRE="${{PWD%%\/VM\/*}}/VM"
+fi
+""".format() + '\n'.join(
+        ['export PYTHONPATH="$PYTHONPATH:${PRE}' + a + '"'
+         for a in sys.path
+         if not 'states/bin' in a 
+            and not 'lib/pymodules' in a
+            and not '/lib/python' in a])
+    + """
+export PYTHONPATH="$(echo "${{PYTHONPATH}}"|sed -e "s/^://g"|sed -e "s/\\\\/\\\\//\\\\//g")"
+""".format()
+))
