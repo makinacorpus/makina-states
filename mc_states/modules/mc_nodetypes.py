818d459e0d2784a9021f6ee4450c36bb6d0e8910
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-01-31 17:07:04 +0100

    typo

--- mc_states/modules/mc_nodetypes.py
+++ mc_states/modules/mc_nodetypes.py
@@ -31,7 +31,7 @@ def settings():
 def registry():
     @mc_states.utils.lazy_subregistry_get(__salt__, __name)
     def _registry(REG):
-        settings_reg = __salt__['mc.{0}.settings'.format(__name)]()
+        settings_reg = __salt__['mc_{0}.settings'.format(__name)]()
         travis = False
         for i in os.environ:
             if 'travis' in i.lower():
