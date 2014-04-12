ca42976d612d3c6ef433e0e9a76d4f9aef25e845
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-04-12 18:15:13 +0200

    continue json refactor

--- files/projects/drupal/local.settings.php
+++ files/projects/drupal/local.settings.php
@@ -1,4 +1,5 @@
 <?php
+{% set data = salt['mc_utils.json_load'](data) %}
 // -----------------------------------------------------------------------------
 // LOCAL SETTINGS: OVERRIDES OF SETTINGS FROM settings.php and set in DATABASE
 // -----------------------------------------------------------------------------
@@ -16,35 +17,35 @@
 //$conf['my_thing_one_enabled'] = false;
 // exemple of another
 //$conf['my_thing_two_enabled'] = true;
-$conf['mc_external_publications_allowed'] = {{ mc_external_publications_allowed }};
+$conf['mc_external_publications_allowed'] = {{ data.mc_external_publications_allowed }};
 // -----------------------------------------------------------------------------
 
 
 // Do not use the new image style token. it removes images from varnish and 
 // breaks some modules
-$conf['image_allow_insecure_derivatives'] = {{ image_allow_insecure_derivatives }};
+$conf['image_allow_insecure_derivatives'] = {{ data.image_allow_insecure_derivatives }};
 
 // AVOID SENDING emails if not in production env -------------------------------
-$conf['reroute_email_enable'] = {{ reroute_email_enabled }};
-$conf['reroute_email_address'] = '{{ reroute_email_address }}';
+$conf['reroute_email_enable'] = {{ data.reroute_email_enabled }};
+$conf['reroute_email_address'] = '{{ data.reroute_email_address }}';
 
 // BASE URL --------------------------------------------------------------------
-$base_url = '{{ base_url }}';
+$base_url = '{{ data.base_url }}';
 // this is a hack for drush in site-install mode, overriding base_url with crap,
 // at least you have a copy in variables, if needed
 $conf['base_url'] = $base_url;
 
 // reduce uneeded queries by increasing the allowed max size of cached sentences (default is 75)
-$conf['locale_cache_length'] = {{ locale_cache_length }}; //204800;
+$conf['locale_cache_length'] = {{ data.locale_cache_length }}; //204800;
 
 // Imagecache
-$conf['image_jpeg_quality'] = {{ image_jpeg_quality }}; //95;
+$conf['image_jpeg_quality'] = {{ data.image_jpeg_quality }}; //95;
 
-{% if with_solr %}
+{% if data.with_solr %}
 // Search Solr  ///////////////////////////////////////////////////////////////
-$conf['solr_default_search_server_host'] = "{{ solr_default_search_server_host }}";
-$conf['solr_default_search_server_port'] = "{{ solr_default_search_server_port }}";
-$conf['solr_default_search_server_path'] = "{{ solr_default_search_server_path }}";
+$conf['solr_default_search_server_host'] = "{{ data.solr_default_search_server_host }}";
+$conf['solr_default_search_server_port'] = "{{ data.solr_default_search_server_port }}";
+$conf['solr_default_search_server_path'] = "{{ data.solr_default_search_server_path }}";
 $conf['search_api_attachments_extract_using'] = "solr";
 {% endif %}
 
@@ -54,20 +55,20 @@ $conf['cron_safe_threshold'] = 0;
 // do not limit time for cron tasks
 $conf['elysia_cron_time_limit'] = 0;
 
-{% if with_smtp %}
+{% if data.with_smtp %}
 // SMTP ///////////////////////////////////////////////////////////////
 $conf['smtp_on'] = 1;
-$conf['smtp_host'] = "{{ smtp_host }}";
-$conf['smtp_from'] = "{{ smtp_from }}";
-$conf['smtp_fromname'] = "{{ smtp_fromname }}";
+$conf['smtp_host'] = "{{ data.smtp_host }}";
+$conf['smtp_from'] = "{{ data.smtp_from }}";
+$conf['smtp_fromname'] = "{{ data.smtp_fromname }}";
 {% endif %}
 
-{% if $with_simpletest %}
+{% if data.$with_simpletest %}
 // Simpletest /////////////////////////////////////////////////////////////////
-$conf['simpletest_remote_url'] = '{{ base_url }}';
+$conf['simpletest_remote_url'] = '{{ data.base_url }}';
 {% endif %}
 
-{% if $with_varnish %}
+{% if data.$with_varnish %}
 // Varnish handling ///////////////////////////////////////////////////////////
 // Tell Drupal it's behind a proxy
 /**
@@ -99,15 +100,15 @@ $conf['reverse_proxy'] = true;
  */
 // Bypass Drupal bootstrap for anonymous users so that Drupal sets max-age=0
 // this setting is the old aggressive mode in D6
-$conf['page_cache_invoke_hooks'] = {{ page_cache_invoke_hooks }};
-$conf['cache'] = {{ cache }}; //should be 1
-$conf['block_cache'] = {{ block_cache }}; // should be 1
+$conf['page_cache_invoke_hooks'] = {{ data.page_cache_invoke_hooks }};
+$conf['cache'] = {{ data.cache }}; //should be 1
+$conf['block_cache'] = {{ data.block_cache }}; // should be 1
 // this will be in Cache-Control: public max-age
-$conf['page_cache_maximum_age'] = {{ page_cache_maximum_age }};
+$conf['page_cache_maximum_age'] = {{ data.page_cache_maximum_age }};
 // Define Drupal cache settings:--------------
 // inactivate database connection if the cache backend doesn't need it
 // (cache_page) if the page is not cached the db connection will be made later
-$conf['page_cache_without_database'] = {{ page_cache_without_database }};
+$conf['page_cache_without_database'] = {{ data.page_cache_without_database }};
 
 /**
  * Set this value if your proxy server sends the client IP in a header other
@@ -128,7 +129,7 @@ $conf['reverse_proxy_header'] = 'HTTP_X_FORWARDED_FOR';
  * reverse proxies. Otherwise, the client could directly connect to
  * your web server spoofing the X-Forwarded-For headers.
  */
-$conf['reverse_proxy_addresses'] = {{ varnish_reverse_proxy_addresses }};
+$conf['reverse_proxy_addresses'] = {{ data.varnish_reverse_proxy_addresses }};
 
 /**
  * Page caching:
@@ -147,18 +148,18 @@ $conf['reverse_proxy_addresses'] = {{ varnish_reverse_proxy_addresses }};
  * HTTP proxy, and bypass the reverse proxy if one is used) in order to avoid
  * getting cached pages from the proxy.
  */
-$conf['omit_vary_cookie'] = {{ omit_vary_cookie }}; // usually false;
+$conf['omit_vary_cookie'] = {{ data.omit_vary_cookie }}; // usually false;
 
 {%   if $with_varnish_module %}
 // VARNISH module
 $conf['cache_backends'][1] = 'sites/all/modules/varnish/varnish.cache.inc';
 $conf['cache_class_cache_page'] = 'VarnishCache';
-$conf['varnish_control_terminal'] = '{{ varnish_control_terminal_string }}';
+$conf['varnish_control_terminal'] = '{{ data.varnish_control_terminal_string }}';
 $conf['varnish_cache_clear'] = 0; //0: none 1: drupal defaults
 $conf['varnish_flush_cron'] = 0; // no flush of varnish when drupal cron runs (heresy!)
-$conf['varnish_socket_timeout'] = {{ varnish_socket_timeout }}; //default is 100-> 100ms; 3000->3s
+$conf['varnish_socket_timeout'] = {{ data.varnish_socket_timeout }}; //default is 100-> 100ms; 3000->3s
 // see /etc/varnish/secret on the varnish server
-$conf['varnish_control_key'] = '{{ varnish_control_key }}';
+$conf['varnish_control_key'] = '{{ data.varnish_control_key }}';
 {%   endif %}
 {% endif %}
 
@@ -170,13 +171,13 @@ $conf['cache_lifetime'] = 0; // 0 is infinite., 10800->6h
 // Also bypass Drupal hardcoded block cache disabled when hook_node_grant
 // is implemented by at least one module
 // See and apply http://drupal.org/node/1930960#comment-7124130
-$conf['block_cache_bypass_node_grants'] = {{ block_cache_bypass_node_grants }}; // usually 1;
+$conf['block_cache_bypass_node_grants'] = {{ data.block_cache_bypass_node_grants }}; // usually 1;
 
 // File system ////////////////////////////////////////////////////////////////
 // Warning: when PHP-FPM is chrooted, we musn't use real absolute path but only
 // "absolute path in the choot"
 $conf['file_directory_path'] = 'site/default/files';
-{% if chrooted %}
+{% if data.chrooted %}
 if (!function_exists('drush_main')) {
   $conf['file_private_path'] = '/var/private';
   $conf['file_directory_temp'] = '/var/tmp';
@@ -187,7 +188,7 @@ if (!function_exists('drush_main')) {
   $conf['file_private_path'] = $root . '/var/private';
   $conf['file_directory_temp'] = $root . '/var/tmp';
   $conf['file_temporary_path'] = $root . '/var/tmp';
-{% if chrooted %}
+{% if data.chrooted %}
 }
 {% endif %}
 
@@ -196,7 +197,7 @@ if (!function_exists('drush_main')) {
 // Enter the number of seconds to set a cookie for when a user submits any form
 // on the website. This will ensure that any page they see after submitting a
 // form will be dynamic and not cached.
-$conf['cookie_cache_bypass_adv_cache_lifetime']= {{ cookie_cache_bypass_adv_cache_lifetime }};
+$conf['cookie_cache_bypass_adv_cache_lifetime']= {{ data.cookie_cache_bypass_adv_cache_lifetime }};
 // Choose whether or not the cookie will be set for just the path the user is
 // viewing when filling out the form, or it it will be set for the entire
 // website.
@@ -217,11 +218,11 @@ $conf['form_cache_retention_time'] = 3600;
 
 // Log level //////////////////////////////////////////////////////////////////
 // 1 means some errors get to the end user, 2 means all erros, 0 none.
-$conf['error_level'] = {{ error_level }}; 
+$conf['error_level'] = {{ data.error_level }}; 
 
 // Compression ////////////////////////////////////////////////////////////////
-$conf['preprocess_css'] = {{ preprocess_css }};
-$conf['preprocess_js'] =  {{ preprocess_js }};
+$conf['preprocess_css'] = {{ data.preprocess_css }};
+$conf['preprocess_js'] =  {{ data.preprocess_js }};
 // compression is done on nginx/apache level NEVER SET THIS to 1!
 $conf['page_compression'] = 0;
 
@@ -238,16 +239,16 @@ $conf['file_chmod_file']=0660;
 // our mkdir commands (chmod is not impacted, but mkdir is...)
 umask(0000);
 
-{% if with_redis %}
+{% if data.with_redis %}
 // Redis Configuration
 define('PREDIS_BASE_PATH','sites/all/libraries/predis/');
 $conf['redis_client_interface'] = 'Predis';
-$conf['redis_client_host'] = '{{ redis_server }}';
-$conf['redis_client_port'] = '{{ redis_port }}';
-$conf['redis_client_base'] = {{ redis_base }};
+$conf['redis_client_host'] = '{{ data.redis_server }}';
+$conf['redis_client_port'] = '{{ data.redis_port }}';
+$conf['redis_client_base'] = {{ data.redis_base }};
 {% endif %}
 
-{% if with_redis_session_proxy %}
+{% if data.with_redis_session_proxy %}
 // SSO  //////////////////////////////////////////////////////////////////////
 // SESSION PROXY: Replace core session management with Session Proxy API.
 $conf['session_inc'] = 'sites/all/modules/session_proxy/session.inc';
@@ -257,7 +258,7 @@ $conf['session_inc'] = 'sites/all/modules/session_proxy/session.inc';
 $conf['session_storage_force_default'] = TRUE;
 {% endif %}
 
-{% if with_redis_locks %}
+{% if data.with_redis_locks %}
 // Redis as Lock Backend
 $conf['lock_inc'] = 'sites/all/modules/redis/redis.lock.inc';
 {% endif %}
@@ -271,88 +272,88 @@ $conf['cache_prefix']['default'] = $cache_domain;
 // Define cache engines:
 //----------------------
 // * database, default, is 'DrupalDatabaseCache'
-{% if with_file_cache_backend %}
+{% if data.with_file_cache_backend %}
 // * filecache drupal7 : 'DrupalFileCache'
 $conf['cache_backends'][2] = 'sites/all/modules/filecache/filecache.inc';
 {% endif %}
-{% if with_memcache_cache_backend %}
+{% if data.with_memcache_cache_backend %}
 // Memcache drupal7 : 'MemCacheDrupal'
 $conf['cache_backends'][3] = 'sites/all/modules/memcache/memcache.inc';
 {% endif %}
-{% if with_mongodb_cache_backend %}
+{% if data.with_mongodb_cache_backend %}
 // Mongodb drupal7 : 'DrupalMongoDBCache'
 $conf['cache_backends'][4] = 'sites/all/modules/mongodb/mongodb_cache/mongodb_cache.inc';
 {% endif %}
-{% if with_redis_cache_backend %}
+{% if data.with_redis_cache_backend %}
 // Redis drupal7 : 'Redis_Cache'
 $conf['cache_backends'][5] = 'sites/all/modules/redis/redis.autoload.inc';
 {% endif %}
-{% if with_apc_cache_backend %}
+{% if data.with_apc_cache_backend %}
 // APC drupal7 : 'Apc_Cache'
 $conf['cache_backends'][6] = 'sites/all/modules/apc/apc_cache.inc';
 {% endif %}
 
-{% if cache %}
+{% if data.cache %}
 // Define cache bins
 //------------------
 //  here's the magic, deporting several cache on the appropriate place
 // cache name |  usage/frequency/size (but may depend on your real cache contents, mode brain on)
 // default    |  any/any/any          select memcache, apc, file or db
-$conf['cache_default_class']          = '{{ cache_bin_default }}';
+$conf['cache_default_class']          = '{{ data.cache_bin_default }}';
 // WARNING: this one is 'cache_class_cache' and not 'cache_class_cache_cache'
 // general    |  all/every/medium     select apc > memcache > file > db
-$conf['cache_class_cache']            = '{{ cache_bin_cache }}';
+$conf['cache_class_cache']            = '{{ data.cache_bin_cache }}';
 // bootstrap  |  all/every/medium     select apc > db
-$conf['cache_class_cache_bootstrap']  = '{{ cache_bin_bootstrap }}';
+$conf['cache_class_cache_bootstrap']  = '{{ data.cache_bin_bootstrap }}';
 // block      |  any/often/small      select memcache > db > file
-$conf['cache_class_cache_block']      = '{{ cache_bin_block }}';
+$conf['cache_class_cache_block']      = '{{ data.cache_bin_block }}';
 // field      |  page/some/large      select file > memcache > db
-$conf['cache_class_cache_content']    = '{{ cache_bin_content }}';
+$conf['cache_class_cache_content']    = '{{ data.cache_bin_content }}';
 // filter     |  page/some/large      select file > memcache > db
-$conf['cache_class_cache_filter']     = '{{ cache_bin_filter }}';
+$conf['cache_class_cache_filter']     = '{{ data.cache_bin_filter }}';
 // form       |  edit/rare/medium     select file > memcache > db
-$conf['cache_class_cache_form']       = '{{ cache_bin_form }}';
+$conf['cache_class_cache_form']       = '{{ data.cache_bin_form }}';
 // menu       |  any/often/large      select memcache > db > file
-$conf['cache_class_cache_menu']       = '{{ cache_bin_menu }}';
+$conf['cache_class_cache_menu']       = '{{ data.cache_bin_menu }}';
 // page       |  page/often/large     select memcache > file > db
-$conf['cache_class_cache_page']       = '{{ cache_bin_page }}';
+$conf['cache_class_cache_page']       = '{{ data.cache_bin_page }}';
 // session    |  any/any/small        select apc (mono server) > memcache > db
-$conf['cache_class_cache_session']    = '{{ cache_bin_session }}';
+$conf['cache_class_cache_session']    = '{{ data.cache_bin_session }}';
 // update     |  system/rare/large,   select file > db
-$conf['cache_class_cache_update']     = '{{ cache_bin_update }}';
+$conf['cache_class_cache_update']     = '{{ data.cache_bin_update }}';
 // users      |  any/some/large       select memcache > file > db
-$conf['cache_class_cache_users']      = '{{ cache_bin_users }}';
+$conf['cache_class_cache_users']      = '{{ data.cache_bin_users }}';
 // views      |  any/some/large       select memcache > file > db
-$conf['cache_class_cache_views']      = '{{ cache_bin_views }}';
+$conf['cache_class_cache_views']      = '{{ data.cache_bin_views }}';
 // views data |  any/often/small      select apc > db
-$conf['cache_class_cache_views_data'] = '{{ cache_bin_views_data }}';
+$conf['cache_class_cache_views_data'] = '{{ data.cache_bin_views_data }}';
 // path
-$conf['cache_class_cache_path'] = '{{ cache_bin_path }}';
+$conf['cache_class_cache_path'] = '{{ data.cache_bin_path }}';
 // tokens
-$conf['cache_class_cache_tokens'] = '{{ cache_bin_tokens }}';
+$conf['cache_class_cache_tokens'] = '{{ data.cache_bin_tokens }}';
 // rules
-$conf['cache_class_cache_rules'] = '{{ cache_bin_rules }}';
+$conf['cache_class_cache_rules'] = '{{ data.cache_bin_rules }}';
 // field
-$conf['cache_class_cache_field'] = '{{ cache_bin_field }}';
+$conf['cache_class_cache_field'] = '{{ data.cache_bin_field }}';
 
 // EntityCache
-$conf['cache_class_cache_entity_comment'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_file'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_node'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_og_membership'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_og_membership_type'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_profile2'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_taxonomy_term'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_taxonomy_vocabulary'] = '{{ cache_bin_entity }}';
-$conf['cache_class_cache_entity_user'] = '{{ cache_bin_entity }}';
+$conf['cache_class_cache_entity_comment'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_file'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_node'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_og_membership'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_og_membership_type'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_profile2'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_taxonomy_term'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_taxonomy_vocabulary'] = '{{ data.cache_bin_entity }}';
+$conf['cache_class_cache_entity_user'] = '{{ data.cache_bin_entity }}';
 {% endif %}
 
-$conf['site_mail'] = '{{ site_email }}';
+$conf['site_mail'] = '{{ data.site_email }}';
 
 // Pure performance: avoid banned ip query during bootstrap.
 $conf['blocked_ips'] = array();
 
-{% if with_local_overrides %}
+{% if data.with_local_overrides %}
 // Local overrides ////////////////////////////////////////////////////////////
 // If you want to alter some default setting, create your
 // overrides.settings.php and put theses settings inside like
