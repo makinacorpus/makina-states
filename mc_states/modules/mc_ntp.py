ebea2fd3b95ce3618400186832627d1b2abaa9df
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-04-24 21:55:36 +0200

    strengthen ntp module

--- mc_states/modules/mc_ntp.py
+++ mc_states/modules/mc_ntp.py
@@ -28,6 +28,17 @@ log = logging.getLogger(__name__)
 def settings():
     '''
     ntp
+
+        servers
+           server entries without the fudge headers
+        fudge
+            fudge entries without the fudge headers
+
+        default_all
+            allow query from ext (firewalled in ms)
+        block_ext
+            block all outbound queryies
+
     '''
     @mc_states.utils.lazy_subregistry_get(__salt__, __name)
     def _settings():
@@ -35,6 +46,20 @@ def settings():
         pillar = __pillar__
         locations = __salt__['mc_locations.settings']()
         ntpData = __salt__['mc_utils.defaults'](
+            'makina-states.services.base.ntp', {
+                'servers': [
+                    '0.ubuntu.pool.ntp.org',
+                    '1.ubuntu.pool.ntp.org',
+                    '2.ubuntu.pool.ntp.org',
+                    '3.ubuntu.pool.ntp.org',
+                    'ntp.ubuntu.com',
+                    '127.127.1.0',
+                ],
+                'fudge': [
+                    '127.127.1.0 stratum 11',
+                ],
+                'default_all': True,
+                'block_ext': False,
             }
         )
         return ntpData
