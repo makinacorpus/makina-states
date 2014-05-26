155a438d6b349c4cc9bbcc78756ab7ccfbee66b1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-05-26 11:07:10 +0200

    wip on ulogd

---
+++ mc_states/modules/mc_ulogd.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''
+.. _module_mc_ulogd:
+
+mc_ulogd / ulogd functions
+==================================
+'''
+
+__docformat__ = 'restructuredtext en'
+# Import python libs
+import logging
+import os
+import mc_states.utils
+
+__name = 'ulogd'
+
+log = logging.getLogger(__name__)
+
+
+def settings():
+    '''
+    ulogd settings
+
+
+    '''
+    @mc_states.utils.lazy_subregistry_get(__salt__, __name)
+    def _settings():
+        grains = __grains__
+        pillar = __pillar__
+        nt_reg = __salt__['mc_nodetypes.registry']()
+        locs = __salt__['mc_locations.settings']()
+        data = __salt__['mc_utils.defaults'](
+            'makina-states.services.log.ulogd', {
+            }
+        )
+        return data
+    return _settings()
+
+
+def dump():
+    return mc_states.utils.dump(__salt__,__name)
+
+#
