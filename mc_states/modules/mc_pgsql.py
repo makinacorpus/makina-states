7e56666ac1effaf84c89f645a7ec34cb948ec7e0
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-06-18 21:26:39 +0200

    add lxc src to pghba

--- mc_states/modules/mc_pgsql.py
+++ mc_states/modules/mc_pgsql.py
@@ -213,6 +213,12 @@ def settings():
                      'user': 'all',
                      'address': '::1/128',
                      'method': 'md5'},
+                    {"comment": '# makina-states lxc inter communication'},
+                    {'type': 'host',
+                     'db': 'all',
+                     'user': 'all',
+                     'address': '10.5.0.0/16',
+                     'method': 'md5'},
                 ]
             }
         )
