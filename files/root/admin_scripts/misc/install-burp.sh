427f83684de879a605f841090e1ed580b7a29d63
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-06-19 14:53:19 +0200

    mode

---
+++ files/root/admin_scripts/misc/install-burp.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+VER"=${BURP_VER:-"1.3.48"}"
+cd $HOME
+if [ ! -d burp ];then
+    git clone git clone https://github.com/grke/burp.git
+fi
+cd burp
+git reset --hard remotes/origin/$VER
+apt-get install -y build-essential  librsync-dev libz-dev libssl-dev uthash-dev libacl1-dev libncurses5-dev
+./configure  && make && make install
