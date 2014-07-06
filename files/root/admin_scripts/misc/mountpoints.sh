660b8175025c3a82f250e2932794242c59161ccd
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-07-06 22:42:46 +0200

    Create mountpoints.sh

---
+++ files/root/admin_scripts/misc/mountpoints.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for i in var/cache/mastersalt var/cache/salt var/lib/docker/ var/lib/lxc;do
+ orig="/srv/mountpoints/${i//\//_}"
+ dest="/$i"
+ mkdir -p "$orig" "$dest"
+ rsync -azv "$dest/" "$orig/"
+ echo "$orig $dest none bind 0 0">>/etc/fstab
+ mount ${orig}
+done
