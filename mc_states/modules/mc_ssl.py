cf69070fd1ef90fac29ee665bcfa90d9b7466655
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-09-17 19:03:02 +0200

    self signed certs

--- mc_states/modules/mc_ssl.py
+++ mc_states/modules/mc_ssl.py
@@ -178,6 +178,32 @@ def get_cert_for(domain, gen=False, domain_csr_data=None):
     return certp, certk
 
 
+def get_selfsigned_cert_for(domain, gen=False, domain_csr_data=None):
+    '''Generate or return certificate for domain'''
+    ensure_ca_present()
+    if domain_csr_data is None:
+        domain_csr_data = {}
+    cloudSettings = __salt__['mc_cloud.settings']()
+    ssl_gen_d = get_selfsigned_certs_dir()
+    certp = os.path.join(ssl_gen_d, 'certs', '{0}.crt'.format(domain))
+    certk = os.path.join(ssl_gen_d, 'certs', '{0}.key'.format(domain))
+    if gen and not os.path.exists(certp):
+        old_d = __opts__.get('ca.cert_base_path', '')
+        try:
+            __salt__['tls.set_ca_path'](ssl_gen_d)
+            domain_csr_data.setdefault('CN', domain)
+            for k, val in cloudSettings['ssl']['ca'].items():
+                if val and (k not in ['CN', 'days', 'ca_name']):
+                    domain_csr_data.setdefault(k, val)
+            ret = __salt__['tls.create_self_signed_cert'](tls_dir='', **domain_csr_data)
+        finally:
+            __salt__['tls.set_ca_path'](old_d)
+    if not os.path.exists(certp):
+        raise CertificateNotFoundError(
+            'Certificate not found for {0}'.format(domain))
+    return certp, certk
+
+
 def is_certificate_matching_domain(cert_path, domain):
     ret = False
     return ret
@@ -257,6 +283,52 @@ def load_certs(path):
     return exacts, alts
 
 
+def load_selfsigned_certs(path):
+    '''
+    Load certificates from a directory (certs must be suffixed with .crt)
+    return 2 dictionnary:
+
+            - one contains certs with common name as indexes
+            - one contains certs with subjectaltnames as indexes
+    '''
+    exacts = {}
+    alts = {}
+    certs_dir = get_selfsigned_certs_dir()
+    if certs_dir and not os.path.exists(certs_dir):
+        os.makedirs(certs_dir)
+    for cert in os.listdir(path):
+        if not cert.endswith('.crt'):
+            continue
+        certp = os.path.join(certs_dir, cert)
+        certk = os.path.join(certs_dir, "{0}.key".format(cert[:-4]))
+        certo = OpenSSL.crypto.load_certificate(
+            OpenSSL.crypto.FILETYPE_PEM, open(certp).read())
+        for i in ("{0}".format(certo.get_subject())).split('/'):
+            data = {'key': certk, 'cert': certp}
+            certo = M2Crypto.X509.load_cert(certp)
+            if i.startswith('CN='):
+                cn = i.split('CN=')[1]
+                if cn not in exacts:
+                    exacts[cn] = data
+        try:
+            ext = certo.get_ext('subjectAltName')
+            for ev in ext.get_value().split(','):
+                if ev.startswith('DNS:'):
+                    cn = ''.join(ev.split('DNS:')[1:])
+                    if cn not in alts:
+                        alts[cn] = data
+        except LookupError:
+            pass
+    return exacts, alts
+
+
+def get_selfsigned_certs_dir():
+    cloudSettings = _salt__['mc_cloud.settings']()
+    ssl_gen_d = cloudSettings['ssl_pillar_dir']
+    certs_dir = os.path.join(ssl_gen_d, 'selfsigned')
+    return certs_dir
+
+
 def get_certs_dir():
     cloudSettings = __salt__['mc_cloud.settings']()
     ca = cloudSettings['ssl']['ca']['ca_name']
@@ -308,6 +380,74 @@ def search_matching_certificate(domain, as_text=False):
         with open(certk) as fic:
             certk = fic.read()
     return certp, certk
+
+
+def search_matching_selfsigned_certificate(domain, as_text=False):
+    '''Search in the pillar certificate directory the
+    certificate belonging to a particular domain'''
+    if not HAS_SSL:
+        raise Exception('Missing pyopenssl')
+    certs_dir = get_selfsigned_certs_dir()
+    certp, certk = None, None
+    # try to get a exact-matchinf filename<->domain
+    try:
+        certp, certk = get_selfsigned_cert_for(domain)
+    except CertificateNotFoundError:
+        pass
+    # parse certificates to see if we can find an exactly but misnamed cert
+    if not certp:
+        exacts, alts = load_selfsigned_certs(certs_dir)
+        for cert_domain in exacts:
+            if domain_match(domain, cert_domain):
+                data = exacts[cert_domain]
+                certp, certk = data['cert'], data['key']
+                break
+    # parse certificates to see if we can find a certificate
+    # with a subjectAltName extension matching our domain
+    if not certp:
+        for cert_domain in alts:
+            if domain_match(domain, cert_domain):
+                data = alts[cert_domain]
+                certp, certk = data['cert'], data['key']
+                break
+    # last resort, try to generate a certificate throught our CA
+    if not certp:
+        certp, certk = get_selfsigned_cert_for(domain, gen=True)
+    if (not certp) or (certp and not os.path.exists(certp)):
+        raise MissingCertError(
+            '{1}: Missing cert: {0}'.format(certp, domain))
+    if (not certk) or (certk and not os.path.exists(certk)):
+        raise MissingKeyError(
+            '{1}: Missing private key for cert: {0}'.format(certp, domain))
+    if as_text:
+        with open(certp) as fic:
+            certp = fic.read()
+        with open(certk) as fic:
+            certk = fic.read()
+    return certp, certk
+
+
+def selfsigned_ssl_certs(domains, as_text=False):
+    '''
+    Maybe Generate
+    and Return SSL certificate and key paths for domain
+    Certicates are generated inside pillar/cloudcontroller/<minionid>.
+    this generates a signed certificate with a generated
+    certificate authority with the name of the current
+    minion.
+    '''
+    if not domains:
+        raise ValueError('domains must be set')
+    if isinstance(domains, basestring):
+        domains = domains.split(',')
+    ssl_certs = []
+    for domain in domains:
+        crt_data = search_matching_selfsigned_certificate(
+            domain, as_text=as_text)
+        if crt_data not in ssl_certs:
+            ssl_certs.append(crt_data)
+    return ssl_certs
+
 
 
 def ssl_certs(domains, as_text=False):
