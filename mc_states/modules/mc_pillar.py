8b34c3f396fd12cbf59d30664b231bf46e47de34
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-10-04 17:29:05 +0200

    pillar stuff

--- mc_states/modules/mc_pillar.py
+++ mc_states/modules/mc_pillar.py
@@ -2540,35 +2540,39 @@ def get_mail_conf(id_):
     mail_conf = __salt__['mc_pillar.get_mail_configuration'](id_)
     dest = mail_conf['default_dest'].format(id=id_)
     data['makina-states.services.mail.postfix'] = True
-    data['makina-states.services.mail.postfix.mode'] = mail_conf['mode']
-    if is_salt_managed(id_) and mail_conf.get('transports'):
-        transports = data.setdefault(
-            'makina-states.services.mail.postfix.transport', [])
-        for entry, host in mail_conf['transports'].items():
-            if entry != '*':
-                transports.append({
-                    'transport': entry,
-                    'nexthop': 'relay:[{0}]'.format(host)})
-        if '*' in mail_conf['transports']:
-            transports.append(
-                {'nexthop':
-                 'relay:[{0}]'.format(mail_conf['transports']['*'])})
-        if mail_conf['auth']:
-            passwds = data.setdefault(
-                'makina-states.services.mail.postfix.sasl_passwd', [])
-            data['makina-states.services.mail.postfix.auth'] = True
-            for entry, host in mail_conf['smtp_auth'].items():
-                passwds.append({
-                    'entry': '[{0}]'.format(entry),
-                    'user': host['user'],
-                    'password': host['password']})
-        if mail_conf.get('virtual_map'):
-            vmap = data.setdefault(
-                'makina-states.services.mail.postfix.virtual_map', {})
-        for record in mail_conf['virtual_map']:
-            for item, val in record.items():
-                vmap[item.format(id=id_, dest=dest)] = val.format(
-                    id=id_, dest=dest)
+    mode = 'makina-states.services.mail.postfix.mode'
+    data[mode] = mail_conf['mode']
+    if is_managed(id_):
+        if is_salt_managed(id_) and mail_conf.get('transports'):
+            transports = data.setdefault(
+                'makina-states.services.mail.postfix.transport', [])
+            for entry, host in mail_conf['transports'].items():
+                if entry != '*':
+                    transports.append({
+                        'transport': entry,
+                        'nexthop': 'relay:[{0}]'.format(host)})
+            if '*' in mail_conf['transports']:
+                transports.append(
+                    {'nexthop':
+                     'relay:[{0}]'.format(mail_conf['transports']['*'])})
+            if mail_conf['auth']:
+                passwds = data.setdefault(
+                    'makina-states.services.mail.postfix.sasl_passwd', [])
+                data['makina-states.services.mail.postfix.auth'] = True
+                for entry, host in mail_conf['smtp_auth'].items():
+                    passwds.append({
+                        'entry': '[{0}]'.format(entry),
+                        'user': host['user'],
+                        'password': host['password']})
+            if mail_conf.get('virtual_map'):
+                vmap = data.setdefault(
+                    'makina-states.services.mail.postfix.virtual_map', {})
+            for record in mail_conf['virtual_map']:
+                for item, val in record.items():
+                    vmap[item.format(id=id_, dest=dest)] = val.format(
+                        id=id_, dest=dest)
+        else:
+            data[mode] = 'localdeliveryonly'
     return data
 
 
