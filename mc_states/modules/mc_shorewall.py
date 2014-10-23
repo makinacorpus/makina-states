380c21b31166633bee6b3046773ef48d76a9db1c
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2014-10-23 16:48:18 +0200

    oups,typo,again

--- mc_states/modules/mc_shorewall.py
+++ mc_states/modules/mc_shorewall.py
@@ -14,6 +14,7 @@ import logging
 import traceback
 from distutils.version import LooseVersion
 import mc_states.utils
+import ipaddr
 from salt.utils.odict import OrderedDict
 
 from mc_states import ping
