b2fc0ecd9b5a03b059bc5b68400b479e735a7b52
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-02-08 17:54:21 +0100

    useless set -e

--- files/etc/burp/clientcagen.sh
+++ files/etc/burp/clientcagen.sh
@@ -4,7 +4,6 @@
 {% set ssdata = settings.server_conf %}
 {% set data=settings[client] %}
 {% set cdata=data %}
-set -e
 if test -e /etc/burp/CA/.{{client}}done;then exit 0;fi
 if test -e /etc/burp/CA/{{cdata.cname}}.crt;then exit 0;fi
 burp_ca --key --request --name {{cdata.cname}} &&\
