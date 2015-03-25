8538dba2d370b408bac5eabb709db4e394337776
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-03-25 02:27:41 +0100

    doc

--- files/etc/network/reconfigure_ifc.sh
+++ files/etc/network/reconfigure_ifc.sh
@@ -2,13 +2,16 @@
 set -x
 ADDR="{{addr}}"
 IFACE="{{ifn}}"
+# test the precense of the interface
 ip addr show dev "${IFACE}" 1>/dev/null 2>/dev/null
 ret=${?}
 if [ "x${ret}" = "x0" ];then
   if [ "x${ADDR}" != "x" ];then
+    # test that the ip is attached
     test "x$(ip addr show dev "${IFACE}"|egrep "inet.*${ADDR}.*scope"|wc -l|sed "s/ //g")" != "x0"
     ret=${?}
   else
+      # in case of real ifs bridged, test that there are no associated ip !
     test "x$(ip addr show dev "${IFACE}"|egrep "inet.*scope"|wc -l|sed "s/ //g")" = "x0"
     ret=${?}
   fi
@@ -20,10 +23,12 @@ fi
 ifdown "${IFACE}"
 ifconfig "${IFACE}" down
 if [ "x${ADDR}" = "x" ];then
+  # in case of real ifs bridged, remove any associated ip !
   for cip in $(ip addr show dev "${IFACE}"|grep "inet "|awk '{print $2}');do
     ip addr del "${cip}" dev "${IFACE}"
   done
 fi
+# let the iface becomes up, this can fails silently on the first try
 ifup "${IFACE}"
 ret=${?}
 ifup "${IFACE}"
