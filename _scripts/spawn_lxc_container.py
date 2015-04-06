fcc21056979f51ff378af8260574f63934a42cb7
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-04-06 23:06:50 +0200

    WIP: restore image

--- _scripts/spawn_lxc_container.py
+++ _scripts/spawn_lxc_container.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division,  print_function
 __docformat__ = 'restructuredtext en'
 
+import glob
 import shutil
 import os
 import time
@@ -99,14 +100,108 @@ def edit_config(lxc_config, bridge, ip, mac):
             fic.write(ocontent)
 
 
-def get_available_mac():
+def get_available_mac(lxc_dir='/var/lib/lxc'):
     '''.'''
-    return '00:16:3e:aa:c0:57'
+    cmacs = []
+    base = '00:16:{0}:{1}:{2}:{3}'
+    pattern = 'lxc.network.hwaddr = '
+    for i in glob.glob('{0}/*/config'.format(lxc_dir)):
+        with open(i) as fic:
+            lines = fic.readlines()
+            cmacs.extend([a.split(pattern, 1)[1].strip()
+                          for a in lines
+                          if a.startswith('{0}'.format(pattern))])
+    cmacs = [re.sub('/.*', '', c) for c in cmacs]
+    mac = None
+    for i in xrange(int('1', 16), int('FF', 16)):
+        for j in xrange(int('1', 16), int('FF', 16)):
+            for k in xrange(int('1', 16), int('FF', 16)):
+                for l in xrange(int('1', 16), int('FF', 16)):
+                    cmac = base.format(i, j,  k, l)
+                    if cmac not in cmacs:
+                        mac = cmac
+                    if mac is not None:
+                        break
+                if mac is not None:
+                    break
+            if mac is not None:
+                break
+        if mac is not None:
+            break
+    if mac is None:
+        raise ValueError('No mac !')
+    return mac
 
 
-def get_available_ip():
+def get_available_ip(lxc_dir='/var/lib/lxc'):
     '''.'''
-    return '10.5.0.6'
+    cipv4s = []
+    pattern = 'lxc.network.ipv4 = '
+    for i in glob.glob('{0}/*/config'.format(lxc_dir)):
+        with open(i) as fic:
+            lines = fic.readlines()
+            cipv4s.extend([a.split(pattern, 1)[1].strip()
+                           for a in lines
+                           if a.startswith('{0}'.format(pattern))])
+    cipv4s = [re.sub('/.*', '', c) for c in cipv4s]
+    ip = None
+    for i in range(0, 253):
+        for j in range(2, 252):
+            cip = '10.5.{0}.{1}'.format(i, j)
+            if cip not in cipv4s:
+                ip = cip
+            if ip is not None:
+                break
+        if ip is not None:
+            break
+    if ip is None:
+        raise ValueError('No ip !')
+    return ip
+
+
+
+def get_container_status(container):
+    ret, ps = popen('lxc-ls --fancy')
+    if ps.returncode:
+        print(ret[0])
+        print(ret[1])
+        print('error while getting lxcls')
+        raise ValueError('lxc ls for {0}'.format(container))
+    out = ret[0] + ret[1]
+    status = None
+    for i in out.splitlines()[2:]:
+        parts = i.split()
+        if parts[0] == container:
+            status = parts[1]
+            break
+    return status
+
+
+def is_stopped(container):
+    return get_container_status(container) == 'STOPPED'
+
+
+def is_started(container):
+    return get_container_status(container) == 'RUNNING'
+
+
+def stop_container(container):
+    if not is_stopped(container):
+        popen('lxc-stop -k -n {0}'.format(container))
+        if not is_stopped(container):
+            raise ValueError('{0} wont stop'.format(container))
+
+
+def start_container(container):
+    if not is_started(container):
+        popen('lxc-start -d -n {0}'.format(container))
+    if not is_started(container):
+        raise ValueError('{0} wont start'.format(container))
+
+
+def restart_container(container):
+    stop_container(container)
+    start_container(container)
 
 
 def main():
@@ -119,7 +214,7 @@ def main():
                         help='LXC top directory (default: /var/lib/lxc)')
     parser.add_argument('-m', '--mac',
                         dest='mac',
-                        default=get_available_mac(),
+                        default=None,
                         help='mac of the new container')
     parser.add_argument('-f', '--force',
                         dest='force',
@@ -129,7 +224,7 @@ def main():
                               ' even if container already exists'))
     parser.add_argument('-i', '--ip',
                         dest='ip',
-                        default=get_available_ip(),
+                        default=None,
                         help='ip of the new container')
     parser.add_argument('-o', '--origin',
                         dest='origin',
@@ -162,21 +257,31 @@ def main():
     arootfs = os.path.join(adir, 'rootfs')
     aconfig = os.path.join(adir, 'config')
     force = opts['force']
-    if opts['snapshots'] not in [None, 'aufs', 'overlayfs']:
+    if opts['snapshot'] not in [None, 'aufs', 'overlayfs']:
         raise ValueError('invalid snapshot type')
     if not os.path.exists(lxc_dir):
         raise ValueError('LXC top dir does not exists, did you installed lxc')
+    if not opts['ip']:
+        opts['ip'] = get_available_ip(lxc_dir)
+    if not opts['mac']:
+        opts['mac'] = get_available_mac(lxc_dir)
     if not (os.path.exists(orootfs) and os.path.exists(oconfig)):
         raise ValueError('Invalid origin container: {0}'.format(odir))
     if 'linux' not in sys.platform.lower():
         raise ValueError('This must be run on linux')
     if os.path.exists(arootfs) and not force:
         raise ValueError('{0} already created'.format(adir))
-    if not os.path.exists(orootfs):
+    if not os.path.exists(arootfs):
         clone_template(opts['origin'], opts['name'], snapshot=opts['snapshot'])
-    if os.path.exists(arootfs) and not force:
-        raise ValueError('{0} already created'.format(adir))
+    if not os.path.exists(arootfs):
+        raise ValueError('{0} does not exists'.format(adir))
     edit_config(aconfig, opts['bridge'], opts['ip'], opts['mac'])
+    restart_container(opts['name'])
+    regen_sshconfig(opts['name'])
+    print('--')
+    print('Your container is in {0}'.format(adir))
+    print('   config: {0}'.format(aconfig))
+    print('   IP: {0}'.format(opts['ip']))
 
 
 if __name__ == '__main__':
