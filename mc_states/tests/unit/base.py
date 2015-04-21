5a9db18a37ebd54177bc93eb4da78e5309f3977b
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-04-22 00:04:49 +0200

    manual tests imports for coverage reporting

--- mc_states/tests/unit/base.py
+++ mc_states/tests/unit/base.py
@@ -8,18 +8,112 @@ try:
 except ImportError:
     import unittest
 
-
 from salt.modules import grains as sgrains
-from mc_states.modules import mc_localsettings as mmc_localsettings
-from mc_states.modules import mc_pillar as mmc_pillar
-from mc_states.modules import mc_utils as mmc_utils
-from mc_states.modules import mc_dumper as mmc_dumper
-from mc_states.modules import mc_locations as mmc_locations
-from mc_states.modules import mc_macros as mmc_macros
-from mc_states.modules import mc_ntp as mmc_ntp
-from mc_states.modules import mc_remote as mmc_remote
 
-from mc_states.renderers import lyaml as mmc_lyaml
+from mc_states.modules import mc_apache as mmc_apache
+from mc_states.modules import mc_autoupgrade as mmc_autoupgrade
+from mc_states.modules import mc_bind as mmc_bind
+from mc_states.modules import mc_bootstraps as mmc_bootstraps
+from mc_states.modules import mc_burp as mmc_burp
+from mc_states.modules import mc_casperjs as mmc_casperjs
+from mc_states.modules import mc_circus as mmc_circus
+from mc_states.modules import mc_cloud as mmc_cloud
+from mc_states.modules import mc_cloud_compute_node as mmc_cloud_compute_node
+from mc_states.modules import mc_cloud_controller as mmc_cloud_controller
+from mc_states.modules import mc_cloud_images as mmc_cloud_images
+from mc_states.modules import mc_cloud_kvm as mmc_cloud_kvm
+from mc_states.modules import mc_cloud_lxc as mmc_cloud_lxc
+from mc_states.modules import mc_cloud_saltify as mmc_cloud_saltify
+from mc_states.modules import mc_cloud_vm as mmc_cloud_vm
+from mc_states.modules import mc_controllers as mmc_controllers
+from mc_states.modules import mc_dbsmartbackup as mmc_dbsmartbackup
+from mc_states.modules import mc_dhcpd as mmc_dhcpd
+from mc_states.modules import mc_dns as mmc_dns
+from mc_states.modules import mc_dumper as mmc_dumper
+from mc_states.modules import mc_env as mmc_env
+from mc_states.modules import mc_etckeeper as mmc_etckeeper
+from mc_states.modules import mc_etherpad as mmc_etherpad
+from mc_states.modules import mc_fail2ban as mmc_fail2ban
+from mc_states.modules import mc_grub as mmc_grub
+from mc_states.modules import mc_haproxy as mmc_haproxy
+from mc_states.modules import mc_icinga2 as mmc_icinga2
+from mc_states.modules import mc_icinga as mmc_icinga
+from mc_states.modules import mc_icinga_web as mmc_icinga_web
+from mc_states.modules import mc_java as mmc_java
+from mc_states.modules import mc_kernel as mmc_kernel
+from mc_states.modules import mc_ldap as mmc_ldap
+from mc_states.modules import mc_locales as mmc_locales
+from mc_states.modules import mc_localsettings as mmc_localsettings
+from mc_states.modules import mc_locations as mmc_locations
+from mc_states.modules import mc_logrotate as mmc_logrotate
+from mc_states.modules import mc_lxc as mmc_lxc
+from mc_states.modules import mc_macros as mmc_macros
+from mc_states.modules import mc_memcached as mmc_memcached
+from mc_states.modules import mc_mongodb as mmc_mongodb
+from mc_states.modules import mc_monitoring as mmc_monitoring
+from mc_states.modules import mc_mumble as mmc_mumble
+from mc_states.modules import mc_mysql as mmc_mysql
+from mc_states.modules import mc_nagvis as mmc_nagvis
+from mc_states.modules import mc_network as mmc_network
+from mc_states.modules import mc_nginx as mmc_nginx
+from mc_states.modules import mc_nodejs as mmc_nodejs
+from mc_states.modules import mc_nodetypes as mmc_nodetypes
+from mc_states.modules import mc_nscd as mmc_nscd
+from mc_states.modules import mc_ntp as mmc_ntp
+from mc_states.modules import mc_pgsql as mmc_pgsql
+from mc_states.modules import mc_phantomjs as mmc_phantomjs
+from mc_states.modules import mc_php as mmc_php
+from mc_states.modules import mc_pillar as mmc_pillar
+from mc_states.modules import mc_pkgs as mmc_pkgs
+from mc_states.modules import mc_pnp4nagios as mmc_pnp4nagios
+from mc_states.modules import mc_postfix as mmc_postfix
+from mc_states.modules import mc_project_1 as mmc_project_1
+from mc_states.modules import mc_project_2 as mmc_project_2
+from mc_states.modules import mc_project as mmc_project
+from mc_states.modules import mc_provider as mmc_provider
+from mc_states.modules import mc_psad as mmc_psad
+from mc_states.modules import mc_pureftpd as mmc_pureftpd
+from mc_states.modules import mc_rabbitmq as mmc_rabbitmq
+from mc_states.modules import mc_rdiffbackup as mmc_rdiffbackup
+from mc_states.modules import mc_redis as mmc_redis
+from mc_states.modules import mc_remote as mmc_remote
+from mc_states.modules import mc_rsyslog as mmc_rsyslog
+from mc_states.modules import mc_rvm as mmc_rvm
+from mc_states.modules import mc_salt as mmc_salt
+from mc_states.modules import mc_screen as mmc_screen
+from mc_states.modules import mc_services as mmc_services
+from mc_states.modules import mc_shorewall as mmc_shorewall
+from mc_states.modules import mc_slapd as mmc_slapd
+from mc_states.modules import mc_snmpd as mmc_snmpd
+from mc_states.modules import mc_ssh as mmc_ssh
+from mc_states.modules import mc_ssl as mmc_ssl
+from mc_states.modules import mc_state as mmc_state
+from mc_states.modules import mc_supervisor as mmc_supervisor
+from mc_states.modules import mc_timezone as mmc_timezone
+from mc_states.modules import mc_tomcat as mmc_tomcat
+from mc_states.modules import mc_ubuntugis as mmc_ubuntugis
+from mc_states.modules import mc_ulogd as mmc_ulogd
+from mc_states.modules import mc_updatedb as mmc_updatedb
+from mc_states.modules import mc_usergroup as mmc_usergroup
+from mc_states.modules import mc_utils as mmc_utils
+from mc_states.modules import mc_uwsgi as mmc_uwsgi
+from mc_states.modules import mc_www as mmc_www    
+
+from mc_states.states import mc_apache as smc_apache
+from mc_states.states import mc_git as smc_git
+from mc_states.states import mc_lxc as smc_lxc
+from mc_states.states import mc_php as smc_php
+from mc_states.states import mc_postgres_database as smc_postgres_database
+from mc_states.states import mc_postgres_extension as smc_postgres_extension
+from mc_states.states import mc_postgres_group as smc_postgres_group
+from mc_states.states import mc_postgres_user as smc_postgres_user
+from mc_states.states import mc_project as smc_project
+from mc_states.states import mc_proxy as smc_proxy
+from mc_states.states import mc_registry as smc_registry
+
+from mc_states.renderers import lyaml as rmc_lyaml
+
+from mc_states.pillar import mc_pillar as pmc_pillar
 
 import mc_states.tests.utils
 
@@ -40,7 +134,7 @@ DUNDERS = {
         'context': {}}}
 DUNDERS['modules'] = copy.deepcopy(DUNDERS['default'])
 DUNDERS['modules']['salt'] = {
-    'lyaml.render': mmc_lyaml.render,
+    'lyaml.render': rmc_lyaml.render,
     'mc_remote.sls': mmc_remote.sls_,
     'mc_remote.highstate': mmc_remote.highstate,
     'mc_remote.salt_call': mmc_remote.salt_call,
