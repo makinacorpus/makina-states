b9e98aacbd953e2bad66f2df739d6def348ae66f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-10 22:56:00 +0200

    better firewall reset

--- files/usr/bin/disable-shorewall.sh
+++ files/usr/bin/disable-shorewall.sh
@@ -20,5 +20,17 @@ if [ "x${todo}" != "x" ];then
     # try to remove shorewall failed acquired lock
     ps aux|grep iptables|grep -- -w|awk '{print $2}'|xargs kill -9
     echo 1>/proc/sys/net/ipv4/ip_forward || /bin/true
+    for i in OUTPUT INPUT FORWARD;do
+        iptables -t filter -P ${i} ACCEPT
+        iptables -t filter -F ${i}
+    done
+    for i in PREROUTING INPUT FORWARD OUTPUT POSTROUTING;do
+        iptables -t mangle -P ${i} ACCEPT
+        iptables -t mangle -F ${i}
+    done
+    for i in PREROUTING INPUT OUTPUT POSTROUTING;do
+        iptables -t nat -P ${i} ACCEPT
+        iptables -t nat -F ${i}
+    done
 fi
 # vim:set et sts=4 ts=4 tw=80:
