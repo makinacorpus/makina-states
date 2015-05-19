55b61ef884bd930a36f52b6656b86e3c1d768e05
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-19 20:19:05 +0200

    Refactor & test ubuntugis
    Signed-off-by: Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>

---
+++ mc_states/tests/unit/modules/ubuntugis_tests.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import unittest
+from .. import base
+import mock
+from mc_states.api import six
+import mc_states.api
+
+
+class TestCase(base.ModuleCase):
+
+    def test_settings(self):
+        func = self._('mc_ubuntugis.settings')
+        with self.patch(
+            funcs={
+                'modules': {
+                    'mc_pkgs.settings':
+                    mock.MagicMock(return_value={
+                        'udist': 'doo',
+                        'ubuntu_lts': 'boo'
+                    })
+                }
+            },
+            grains={
+                'os_family': 'Debian',
+                'os': 'Debian',
+                'osrelease': 'jessie',
+            },
+            filtered=['mc.*'],
+            kinds=['modules']
+        ):
+            mc_states.api.invalidate_memoize_cache('localreg_ubuntugis_settings')
+            ret = func()
+            self.assertEqual(dict(ret),
+                             {'ppa': 'stable', 'dist': 'boo', 'ubuntu_ppa': 'stable'})
+        with self.patch(
+            funcs={
+                'modules': {
+                    'mc_pkgs.settings':
+                    mock.MagicMock(return_value={
+                        'udist': 'doo',
+                        'ubuntu_lts': 'boo'
+                    })
+                }
+            },
+            grains={
+                'os_family': 'Debian',
+                'os': 'Ubuntu',
+                'osrelease': '12.04',
+            },
+            filtered=['mc.*'],
+            kinds=['modules']
+        ):
+            mc_states.api.invalidate_memoize_cache('localreg_ubuntugis_settings')
+            ret = func()
+            self.assertEqual(dict(ret),
+                             {'ppa': 'stable', 'dist': 'doo', 'ubuntu_ppa': 'stable'})
+        with self.patch(
+            funcs={
+                'modules': {
+                    'mc_pkgs.settings':
+                    mock.MagicMock(return_value={
+                        'udist': 'doo',
+                        'ubuntu_lts': 'boo'
+                    })
+                }
+            },
+            grains={
+                'os_family': 'Debian',
+                'os': 'Ubuntu',
+                'osrelease': '14.04',
+            },
+            filtered=['mc.*'],
+            kinds=['modules']
+        ):
+            mc_states.api.invalidate_memoize_cache('localreg_ubuntugis_settings')
+            ret = func()
+            self.assertEqual(dict(ret),
+                             {'ppa': 'unstable', 'dist': 'doo', 'ubuntu_ppa': 'ubuntu-unstable'})
+
+if __name__ == '__main__':
+    unittest.main()
+# vim:set et sts=4 ts=4 tw=80:
--- services/gis/ubuntugis/prerequisites.sls
+++ services/gis/ubuntugis/prerequisites.sls
@@ -1,26 +1,12 @@
 include:
   - makina-states.services.gis.ubuntugis.hooks
   - makina-states.services.db.postgresql.hooks
-
-{% set pkgssettings = salt['mc_pkgs.settings']() %}
 {% set settings = salt['mc_ubuntugis.settings']() %}
-{% if grains['os_family'] in ['Debian'] %}
-{% set dist = pkgssettings.udist %}
-{% endif %}
-{% if grains['os'] in ['Debian'] %}
-{% set dist = pkgssettings.ubuntu_lts %}
-{% endif %}
-{% if settings.ppa == 'unstable' %}
-{%  set ppa = 'ubuntugis-unstable' %}
-{% elif settings.ppa == 'testing' %}
-{%  set ppa = 'ubuntugis-testing' %}
-{% else %}
-{%  set ppa ='ppa'%}
-{% endif %}
-{% if dist in ['trusty', 'vivid'] %}
-{%  set ppa = 'ubuntugis-unstable' %}
-{% endif %}
+{% set pkgssettings = salt['mc_pkgs.settings']() %}
+{% set ppa = settings.ubuntu_ppa %}
+{% set dist = settings.dist %}
 
+{% if grains['os_family'] in ['Debian'] %}
 ubuntugis-base:
   pkgrepo.managed:
     - humanname: ubuntugis ppa
@@ -34,6 +20,13 @@ ubuntugis-base:
       - pkg: ubuntugis-pkgs
 
 ubuntugis-pgrouting-base:
+{% if grains.get('osrelease') > '14.04' %}
+  file.absent:
+    - names:
+      - {{ salt['mc_locations.settings']().conf_dir }}/apt/sources.list.d/pgrouting.list
+    - watch_in:
+      - pkg: ubuntugis-pkgs
+{% else %}
   pkgrepo.managed:
     - humanname: ubuntugis pgrouting ppa
     - name: deb http://ppa.launchpad.net/georepublic/pgrouting/ubuntu {{dist}} main
@@ -44,11 +37,15 @@ ubuntugis-pgrouting-base:
       - mc_proxy: makina-postgresql-post-pkg
     - watch_in:
       - pkg: ubuntugis-pkgs
+{% endif %}
+{% endif %}
 
 ubuntugis-pkgs:
   pkg.{{salt['mc_pkgs.settings']()['installmode']}}:
     - pkgs:
-      - libgeos-dev
+    {% for i in settings.pkgs %}
+      - "{{i}}"
+    {% endfor %}
     - watch:
       - mc_proxy: ubuntugis-pre-install-hook
     - watch_in:
