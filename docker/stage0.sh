eb2fe07a89a0a41549c1cabeddf87476e5ad5a4f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-06-11 21:09:22 +0200

    WIP: docker images

--- docker/stage0.sh
+++ docker/stage0.sh
@@ -28,25 +28,25 @@ MS_IMAGE="${MS_IMAGE:-"makinacorpus/makina-states-${MS_OS}-${MS_OS_RELEASE}"}"
 docker build -t "${STAGE0_TAG}" -f "${MS_DOCKERFILE}"
 die_in_error "${MS_IMAGE}: stage0 didnt build"
 docker run --privileged -ti --rm \
-    -v /usr/bin/docker:/usr/bin/docker:ro \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /var/run/docker:/var/run/docker \
-    -v /var/lib/docker:/var/lib/docker \
-    -v /sys/fs:/sys/fs:ro \
+    -e MS_GIT_BRANCH="${MS_GIT_BRANCH}" \
+    -e MS_GIT_URL="${MS_GIT_URL}" \
+    -e MS_IMAGE="${MS_IMAGE}" \
+    -e MS_OS_MIRROR="${MS_OS_MIRROR}" \
     -e MS_OS="${MS_OS_RELEASE}" \
     -e MS_OS_RELEASE="${MS_OS_RELEASE}" \
-    -e MS_OS_MIRROR="${MS_OS_MIRROR}" \
     -e MS_OS_RELEASE="${MS_OS_RELEASE}" \
     -e MS_STAGE0_TAG="${MS_STAGE0_TAG}" \
-    -e MS_IMAGE="${MS_IMAGE}" \
-    -e MS_GIT_URL="${MS_GIT_URL}" \
-    -e MS_GIT_BRANCH="${MS_GIT_BRANCH}" \
     -v "${MS_DATA_DIR}":/data \
     -v "${MS_DOCKERFILE}":/bootstrap_scripts/Dockerfile \
     -v "${MS_DOCKER_STAGE0}":/bootstrap_scripts/stage0.sh \
     -v "${MS_DOCKER_STAGE1}":/bootstrap_scripts/stage1.sh \
     -v "${MS_DOCKER_STAGE2}":/bootstrap_scripts/stage2.sh \
     -v "${MS_DOCKER_STAGE3}":/bootstrap_scripts/stage3.sh \
+    -v /sys/fs:/sys/fs:ro \
+    -v /usr/bin/docker:/usr/bin/docker:ro \
+    -v /var/lib/docker:/var/lib/docker \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /var/run/docker:/var/run/docker \
     ${MS_DOCKER_ARGS} "${@}" "${MS_STAGE0_TAG}" /bootstrap_scripts/stage1.sh
 die_in_error "${MS_IMAGE}: upper stages failed, see logs"
 # vim:set et sts=4 ts=4 tw=80:
