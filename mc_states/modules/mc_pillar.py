ba0fb1001423bcd8c25f06593384ae2c2dd860ba
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-09-23 21:10:26 +0200

    harden ssl

--- mc_states/modules/mc_pillar.py
+++ mc_states/modules/mc_pillar.py
@@ -2356,7 +2356,9 @@ def get_supervision_conf_kind(id_, kind, ttl=PILLAR_TTL):
                     nginx = rdata.setdefault('nginx', {})
                     domain = rdata.get('nginx', {}).get('domain', id_)
                     cert, key = __salt__[
-                        'mc_ssl.selfsigned_ssl_certs'](domain, True)[0]
+                        'mc_ssl.selfsigned_ssl_certs'](domain,
+                                                       gen=True,
+                                                       as_text=True)[0]
                     # unknown ca signed certs do not work in nginx
                     # cert, key = __salt__['mc_ssl.ssl_certs'](domain, True)[0]
                     # nginx['ssl_cacert'] = __salt__['mc_ssl.get_cacert'](True)
