1719bdd0b8598b2e731f19c6a2c362c6155a8d80
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-10-17 23:13:07 +0200

    docker stuff

--- _scripts/boot-salt.sh
+++ _scripts/boot-salt.sh
@@ -1075,7 +1075,7 @@ set_vars() {
     VENV_URL="${VENV_URL:-"https://github.com/makinacorpus/makina-states/releases/download/attachedfiles/virtualenv-makina-states-${SALT_BOOT_OS}-${DISTRIB_CODENAME}-stable.tar.xz"}"
     declare -A VENV_URLS_MD5
     VENV_URLS_MD5[ahttps://github.com/makinacorpus/makina-states/releases/download/attachedfiles/virtualenv-makina-states-ubuntu-vivid-stable.tar.xz]="940c8d19e6b7d25dffb091634ddf629a"
-    VENV_URLS_MD5[ahttps://github.com/makinacorpus/makina-states/releases/download/attachedfiles/virtualenv-makina-states-ubuntu-trusty-stable.tar.xz]="940c8d19e6b7d25dffb091634ddf629a"
+    VENV_URLS_MD5[ahttps://github.com/makinacorpus/makina-states/releases/download/attachedfiles/virtualenv-makina-states-ubuntu-trusty-stable.tar.xz]="4a8a83c02846770b4686b4b01b9b8e18"
     VENV_MD5=${VENV_MD5:-${VENV_URLS_MD5[${VENV_URL}]}}
     # export variables to support a restart
 
