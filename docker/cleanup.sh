d79c79f56d35489b478610c2189834231e7dd603
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-10-19 01:01:45 +0200

    docker stuff

--- docker/cleanup.sh
+++ docker/cleanup.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
-docker rm -f $(docker ps -a|grep  Exited|grep "sh -c 'echo" |awk '{print $1}')
-docker rm -f $(docker ps -a|grep  Exited|grep "sh -c '#(nop)" |awk '{print $1}')
-docker rm -f $(docker ps -a|grep  Exited|grep '"bash"' |awk '{print $1}')
-docker rmi $(docker images --filter dangling=true -q)
+# try to cleanup most of the dangling containers and images
+docker ps -a|grep  Exited|grep "sh -c 'echo" |awk '{print $1}'|while read i;do
+    docker rm -f $i
+done
+docker ps -a|grep  Exited|grep "sh -c '#(nop)" |awk '{print $1}'|while read i;do
+    docker rm -f $i
+done
+docker ps -a|grep  Exited|grep '"bash"'\
+    | egrep -v "mysql|postgresql|pgsql|redis|mongodb|storage|backup"\
+    | awk '{print $1}'|while read i;do
+    docker rm -f $i
+done
+docker images --filter dangling=true -q|while read i;do
+    docker rmi $i
+done
 # vim:set et sts=4 ts=4 tw=80:
