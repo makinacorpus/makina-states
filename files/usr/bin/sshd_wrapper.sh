9006b2c01b01cc916f9c285df7d328ab15e4391c
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-10-28 22:47:15 +0100

    fix wrapper

--- files/usr/bin/sshd_wrapper.sh
+++ files/usr/bin/sshd_wrapper.sh
@@ -8,16 +8,16 @@ for i in ${SSHD} $(which sshd) /usr/sbin/sshd;do
     fi
 done
 set -x
-if [ ! -d /etc/ssh/ssh_host_rsa_key ];then
+if [ ! -f /etc/ssh/ssh_host_rsa_key ];then
     ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa -b 4096 || /bin/true
 fi
-if [ ! -d /etc/ssh/ssh_host_dsa_key ];then
+if [ ! -f /etc/ssh/ssh_host_dsa_key ];then
     ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa -b 1024 || /bin/true
 fi
 if [ ! -f /etc/ssh/ssh_host_ed25519_key ];then
     ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519 || /bin/true
 fi
-if [ ! -f /etc/ssh/ssh_host_ecdsa_key];then
+if [ ! -f /etc/ssh/ssh_host_ecdsa_key ];then
     ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key   -N '' -t  ecdsa  || /bin/true
 fi
 exec "${SSHD}" "${@}"
