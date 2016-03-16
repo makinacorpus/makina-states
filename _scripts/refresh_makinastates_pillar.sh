5f5a66242bef62f18bde02f2c8a1fbc399186441
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-03-16 22:04:22 +0100

    doc for script

--- _scripts/refresh_makinastates_pillar.sh
+++ _scripts/refresh_makinastates_pillar.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# cron for using to refresh salt to ansible pillar data
+# like
+# /etc/cron.d/refresh_ansible
+# 15,30,45,00 * * * * root /srv/makina-states/_scripts/refresh_makinastates_pillar.sh
+
 cd $(dirname "${0}")/..
 if [  -f venv ]; then
     . bin/venvactivate
