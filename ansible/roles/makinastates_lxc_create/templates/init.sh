a1ffa8ab995389fc69bc6d51ed089efc7b811f73
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-04-18 21:43:32 +0200

    lxc container creation now works

--- ansible/roles/makinastates_lxc_create/templates/init.sh
+++ ansible/roles/makinastates_lxc_create/templates/init.sh
@@ -2,10 +2,30 @@
 export DEBIAN_FRONTEND noninteractive
 if [ ! -d /root/.ssh ];then mkdir -p /root/.ssh;fi
 if [ ! -e /root/.ssh/authorized_keys ];then touch /root/.ssh/authorized_keys;fi
-if ! which python 2>/dev/null;then
+retry() {
+    tries=$1
+    step=$2
+    shift;shift
+    while true;do
+        "${@}"
+        ret=$?
+        if [ "x${ret}" = "x0" ];then
+            break
+        else
+            sleep $step
+            tries=$(($tries -1))
+            if [[ ${tries} -lt 0 ]];then
+                break
+            fi
+        fi
+    done
+    return $ret
+}
+retry 15 1 ping -q -W 4 -c 1 8.8.8.8
+if ! which python >/dev/null 2>/dev/null;then
     if lsb_release -i -s | egrep -iq "ubuntu|debian";then
-      apt-get update
-      apt-get install -y --force-yes python
+      retry 15 1 apt-get update
+      retry 15 1 apt-get install -y --force-yes python
     fi
 fi
 # vim:set et sts=4 ts=4 tw=80:
--- ansible/roles/makinastates_lxc_create/templates/prereqs.sh
+++ ansible/roles/makinastates_lxc_create/templates/prereqs.sh
@@ -2,8 +2,7 @@
 set -ex
 cd $(dirname $0)
 cat init.sh | lxc-attach -n {{lxc_container_name}} -- tee /tmp/init.sh
-lxc-attach -n {{lxc_container_name}} -- chmod +x /tmp/init.sh
-lxc-attach -n {{lxc_container_name}} -- /tmp/init.sh
+lxc-attach -n {{lxc_container_name}} -- bash /tmp/init.sh
 cat /tmp/ansible_master.pub | \
     lxc-attach -n {{lxc_container_name}} -- tee -a /root/.ssh/authorized_keys
 # vim:set et sts=4 ts=4 tw=80:
