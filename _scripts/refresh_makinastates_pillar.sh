0d2e85cbc3167055616cc7917e459a6c7220d536
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-04-27 09:45:53 +0000

    better inventory wrapper args handling

--- _scripts/refresh_makinastates_pillar.sh
+++ _scripts/refresh_makinastates_pillar.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
-# cron for using to refresh salt to ansible pillar data
-# like
-# /etc/cron.d/refresh_ansible
-# 15,30,45,00 * * * * root /srv/makina-states/_scripts/refresh_makinastates_pillar.sh
-
+# this can be used:
+# - as a wrapper to test the inventory
+# - as a cron for using to refresh the whole salt infra  to ansible pillar data
+#   # /etc/cron.d/refresh_ansible
+#   15,30,45,00 * * * * root /srv/makina-states/_scripts/refresh_makinastates_pillar.sh
 cd $(dirname "${0}")/..
 if [  -f venv ]; then
     . bin/venvactivate
 fi
-python ansible/inventories/makinastates.py ${@:-"--list --refresh-cache"}
+if [[ -n ${ANSIBLE_TARGETS-} ]];then
+    export ANSIBLE_TARGETS="${ANSIBLE_TARGETS-}"
+fi
+args=${@:-"--list --refresh-cache"} 
+python ansible/inventories/makinastates.py $args
 # vim:set et sts=4 ts=4 tw=80:
