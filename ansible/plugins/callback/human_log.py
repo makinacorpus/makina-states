660c82d96fffa72a60e9fcf1ada2e614d9a408de
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-05-12 19:30:57 +0200

    final human log ansible plugin

--- ansible/plugins/callback/human_log.py
+++ ansible/plugins/callback/human_log.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 # https://raw.githubusercontent.com/n0ts/ansible-human_log/master/human_log.py
 # Inspired from: https://github.com/redhat-openstack/khaleesi/blob/master/plugins/callbacks/human_log.py
 # Further improved support Ansible 2.0
@@ -5,77 +10,174 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
+import six
+import datetime
+import pprint
+
 try:
     import simplejson as json
 except ImportError:
     import json
 
+try:
+    import chardet
+    HAS_CHARDET = True
+except ImportError:
+    HAS_CHARDET = False
+
+
+def magicstring(thestr):
+    """
+    Convert any string to UTF-8 ENCODED one
+    """
+    if not HAS_CHARDET:
+        log.error('No chardet support !')
+        return thestr
+    seek = False
+    if (
+        isinstance(thestr, (int, float, long,
+                            datetime.date,
+                            datetime.time,
+                            datetime.datetime))
+    ):
+        thestr = "{0}".format(thestr)
+    if isinstance(thestr, unicode):
+        try:
+            thestr = thestr.encode('utf-8')
+        except Exception:
+            seek = True
+    if seek:
+        try:
+            detectedenc = chardet.detect(thestr).get('encoding')
+        except Exception:
+            detectedenc = None
+        if detectedenc:
+            sdetectedenc = detectedenc.lower()
+        else:
+            sdetectedenc = ''
+        if sdetectedenc.startswith('iso-8859'):
+            detectedenc = 'ISO-8859-15'
+
+        found_encodings = [
+            'ISO-8859-15', 'TIS-620', 'EUC-KR',
+            'EUC-JP', 'SHIFT_JIS', 'GB2312', 'utf-8', 'ascii',
+        ]
+        if sdetectedenc not in ('utf-8', 'ascii'):
+            try:
+                if not isinstance(thestr, unicode):
+                    thestr = thestr.decode(detectedenc)
+                thestr = thestr.encode(detectedenc)
+            except Exception:
+                for idx, i in enumerate(found_encodings):
+                    try:
+                        if not isinstance(thestr, unicode) and detectedenc:
+                            thestr = thestr.decode(detectedenc)
+                        thestr = thestr.encode(i)
+                        break
+                    except Exception:
+                        if idx == (len(found_encodings) - 1):
+                            raise
+    if isinstance(thestr, unicode):
+        thestr = thestr.encode('utf-8')
+    thestr = thestr.decode('utf-8').encode('utf-8')
+    return thestr
+
+
 # Fields to reformat output for
-FIELDS = ['cmd', 'command', 'start', 'end', 'delta', 'msg', 'stdout',
-          'stderr', 'results']
+FIELDS = ['cmd',
+          'command',
+          'start',
+          'end',
+          'delta',
+          'msg',
+          'stdout',
+          'stderr',
+          'salt_out',
+          'saltout',
+          'results']
+
+
+def nlstrip(val):
+    if not val.startswith('\n'):
+        val = val.lstrip()
+    return val
+
+
+
+def indent_string(val, indent_level, indenter=' '):
+    indent = indenter * indent_level
+    out = []
+    for i in val.splitlines():
+        out.append('{0}{1}'.format(indent, magicstring(i)))
+    return '\n'.join(out)
 
 
 class CallbackModule(object):
-
-    """
+    '''
     Ansible callback plugin for human-readable result logging
-    """
+    '''
+
     CALLBACK_VERSION = 2.0
     CALLBACK_TYPE = 'notification'
     CALLBACK_NAME = 'human_log'
     CALLBACK_NEEDS_WHITELIST = False
 
-    def human_log(self, data):
-        if type(data) == dict:
-            for field in FIELDS:
-                no_log = data.get('_ansible_no_log')
-                if field in data.keys() and data[field] and no_log != True:
-                    output = self._format_output(data[field])
-                    print("\n{0}: {1}".format(field, output.replace("\\n","\n")))
-
-    def _format_output(self, output):
+    def _output(self, val, indent=1):
+        out, end = '', None
         # Strip unicode
-        if type(output) == unicode:
-            output = output.encode('ascii', 'replace')
-
-        # If output is a dict
-        if type(output) == dict:
-            return json.dumps(output, indent=2)
-
-        # If output is a list of dicts
-        if type(output) == list and type(output[0]) == dict:
-            # This gets a little complicated because it potentially means
-            # nested results, usually because of with_items.
-            real_output = list()
-            for index, item in enumerate(output):
-                copy = item
-                if type(item) == dict:
-                    for field in FIELDS:
-                        if field in item.keys():
-                            copy[field] = self._format_output(item[field])
-                real_output.append(copy)
-            return json.dumps(output, indent=2)
-
-        # If output is a list of strings
-        if type(output) == list and type(output[0]) != dict:
-            # Strip newline characters
-            real_output = list()
-            for item in output:
-                if "\n" in item:
-                    for string in item.split("\n"):
-                        real_output.append(string)
-                else:
-                    real_output.append(item)
-
-            # Reformat lists with line breaks only if the total length is
-            # >75 chars
-            if len("".join(real_output)) > 75:
-                return "\n" + "\n".join(real_output)
+        if isinstance(val, dict):
+            out += '{'
+            for i, val2 in six.iteritems(val):
+                if not out.endswith('\n'):
+                    out += '\n'
+                out += indent_string('{0}: '.format(i), indent+1)
+                out += nlstrip(self._output(val2, indent+2))
+            end = '}'
+        elif isinstance(val, (list, tuple, set)):
+            out += '['
+            for i in val:
+                if not out.endswith('\n'):
+                    out += '\n'
+                out += indent_string(
+                    '- {0}'.format(nlstrip(self._output(i))), indent+1)
+            end = ']'
+        else:
+            try:
+                sindent = indent
+                sval = "{0}".format(val)
+                if sval.count('\n') > 1 and indent > 1:
+                    # dict/iterable element, we nest an indented string for
+                    # better output
+                    out += '\n'
+                    sindent += 1
+                out += indent_string(sval, sindent)
+            except (Exception,) as exc:
+                try:
+                    out += '<NOT FORMATABLE VALUE: {0}>'.format(exc)
+                except Exception:
+                    out += '<NOT FORMATABLE VALUE>'
+        if end:
+            if val:
+                if not out.endswith('\n'):
+                    out += '\n'
+                out += indent_string(end, indent-1)
             else:
-                return " ".join(real_output)
+                out += end
+        return out
 
-        # Otherwise it's a string, (or an int, float, etc.) just return it
-        return str(output)
+    def human_log(self, data):
+        if isinstance(data, dict):
+            ndata = {}
+            for field, val in six.iteritems(data):
+                if field not in FIELDS:
+                    continue
+                no_log = d_no_log = data.get('_ansible_no_log')
+                if isinstance(val, dict):
+                    no_log = val.get('_ansible_no_log', d_no_log)
+                if not no_log:
+                    ndata[field] = val
+            if ndata:
+                print(self._output(ndata))
 
     def on_any(self, *args, **kwargs):
         pass
