b6a8d81a66dc8eef70a4229dfd9a8a146ac3df75
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-05-16 23:35:53 +0200

    lock on ansible cron

--- _scripts/refresh_makinastates_pillar.sh
+++ _scripts/refresh_makinastates_pillar.sh
@@ -6,11 +6,24 @@
 #   15,30,45,00 * * * * root /srv/makina-states/_scripts/refresh_makinastates_pillar.sh
 cd $(dirname "${0}")/..
 if [  -f venv ]; then
-    . bin/venvactivate
+    . venv/bin/activate
 fi
+lock="$(pwd)/var/ansible/refrech_cache.lock"
+log="$(pwd)/var/ansible/refrech_cache.log"
+find "${lock}" -type f -mmin +30 -delete 1>/dev/null 2>&1
+if [ -e "${lock}" ];then
+  echo "Locked ${0}";exit 1
+fi
+touch "${lock}"
 if [[ -n ${ANSIBLE_TARGETS-} ]];then
     export ANSIBLE_TARGETS="${ANSIBLE_TARGETS-}"
 fi
-args=${@:-"--list --refresh-cache"} 
-python ansible/inventories/makinastates.py $args
+export MS_REFRESH_CACHE="${MS_REFRESH_CACHE-y}"
+args=${@:-"--list"}
+python ansible/inventories/makinastates.py $args 1>$log 2>&1
+ret=${?}
+if [ "x${ret}" != "x0" ];then
+  cat "${log}"
+fi
+rm -f "${lock}"
 # vim:set et sts=4 ts=4 tw=80:
