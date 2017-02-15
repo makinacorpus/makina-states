4f459c754f170f0262d6f0de24e72d8ce0de9704
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-02-15 11:44:30 +0100

    ntp scripts

--- files/sbin/ntp-sync.sh
+++ files/sbin/ntp-sync.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# MANAGED VIA SALT - DO NOT EDIT
-NTPSERVERS="ntp.org"
+NTPSERVERS="${NTPSERVERS:-ntp.org}"
 if [ -f /etc/default/ntpdate ];then
      . /etc/default/ntpdate
 fi
@@ -8,19 +7,23 @@ if [ "x${DEBUG}" != "x" ];then
     set -x
 fi
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/sbin:/sbin:/bin:${PATH}
-is_lxc() {
-    echo  "$(cat -e /proc/1/environ |grep container=lxc|wc -l|sed -e "s/ //g")"
-}
 
+is_container() {
+    if cat -e /proc/1/cgroup 2>/dev/null|egrep -q 'docker|lxc'; then
+        return 0
+    else
+        return 1
+    fi
+}
 filter_host_pids() {
     pids=""
-    if [ "x$(is_lxc)" != "x0" ];then
+    if is_container;then
         pids="${pids} $(echo "${@}")"
     else
         for pid in ${@};do
-            if [ "x$(grep -q lxc /proc/${pid}/cgroup 2>/dev/null;echo "${?}")" != "x0" ];then
+            if ! egrep -q "/(docker|lxc)/" /proc/${pid}/cgroup 2>/dev/null;then
                 pids="${pids} $(echo "${pid}")"
-             fi
+            fi
          done
     fi
     echo "${pids}" | sed -e "s/\(^ \+\)\|\( \+$\)//g"
@@ -33,10 +36,9 @@ else
 fi
 
 pids=$(filter_host_pids $(ps aux|grep ntpd|grep -v grep|awk '{print $2}'))
-if [ "x${NTPSYNC}" != "xno" ] && [ "x$(is_lxc)" = "x0" ] && [ "x${pids}" != "x" ];then
+if [ "x${NTPSYNC}" != "xno" ] && [ "x$(is_container)" = "x0" ] && [ "x${pids}" != "x" ];then
     ${svc}ntp stop 2>&1
     ntpdate "${NTPSERVERS}" 2>&1
     ${svc}ntp start 2>&1
 fi
-echo "changed=false"
 # vim:set et sts=4 ts=4 tw=80:
