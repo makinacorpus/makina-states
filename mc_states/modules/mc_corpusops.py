7061f2c3e43dcd612d5c9474e0f48da39bbe77db
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-05-08 20:28:27 +0100

    Monit corpusops based backups

--- mc_states/modules/mc_corpusops.py
+++ mc_states/modules/mc_corpusops.py
@@ -13,6 +13,7 @@ import ruamel.yaml as _y
 import re
 import os
 import logging
+from salt.utils import yamldumper
 from cStringIO import StringIO
 from salt.utils.odict import OrderedDict
 re_flags = re.M | re.U | re.S
@@ -42,10 +43,12 @@ lxcs_makinastates:
 {id_}_and_lxcs:
   children:
     {id_}_lxcs:
+  vars:
+    lxc_compute_node: {id_}
   hosts:
     {id_}:
 {id_}_lxcs_makinastates:
-  vars: {{ssh_bastion: {id_}}}
+  vars: {{}}
   hosts: {{}}
 '''
     DEFAULT_COPS_INV += '''
@@ -92,6 +95,16 @@ def export_lxc_compute_node(cfg, id_):
                 vmdata['{0}_link'.format(ethx)] = ethxd['link']
 
 
+def remove_dict_attrs(d, attrs):
+    for k in [a for a in d]:
+        if isinstance(d[k], dict):
+            remove_dict_attrs(d[k], attrs)
+        else:
+            if k in attrs:
+                d.pop(k, None)
+    return d
+
+
 def export_compute_node(id_, out_file=None, lxc=False):
     '''Generate corpusops inventory slug to paste
     inside corpusops compatible inventoy'''
@@ -116,16 +129,14 @@ def export_compute_node(id_, out_file=None, lxc=False):
         export_lxc_compute_node(cfg, id_)
     tips = _s['mc_pillar.ips_for'](id_, fail_over=True)
     if tips:
-        ip = tips[0]
+        ip = tips.pop(0)
+        tips.insert(0, '{{public_ip}}')
         cfg['all']['hosts'][id_]['ansible_host'] = ip
-        if len(tips) > 1:
-            if lxc:
-                hvars = cfg['{}_and_lxcs'.format(id_)].setdefault('vars', {})
-                hvars['public_ips'] = tips
-                hvars['public_ip'] = "{{public_ips[0]}}"
-            else:
-                cfg['all']['hosts'][id_]['public_ips'] = tips
-                cfg['all']['hosts'][id_]['public_ip'] = "{{public_ips[0]}}"
+        cfg['all']['hosts'][id_]['public_ip'] = ip
+        if lxc:
+            hvars = cfg['{}_and_lxcs'.format(id_)].setdefault('vars', {})
+            hvars['public_ips'] = tips
+            hvars['public_ip'] = ip
     if out_file and os.path.exists(out_file):
         with open(out_file) as fic:
             content = fic.read()
@@ -135,6 +146,7 @@ def export_compute_node(id_, out_file=None, lxc=False):
         yaml.explicit_start = True
         yaml.default_flow_style = False
         ncfg = _s['mc_utils.dictupdate'](ncfg, cfg)
+        remove_dict_attrs(ncfg, ['ssh_bastion'])
         s = StringIO()
         yaml.dump(ncfg, s)
         dcfg = s.getvalue()
@@ -145,6 +157,8 @@ def export_compute_node(id_, out_file=None, lxc=False):
     dcfg = dcfg.replace('burpclientserver_profiles_vm\']}\n',
                         'burpclientserver_profiles_vm\']}}\n')
     dcfg = re.sub('(: {ssh_bastion[^}]+)}.$', '\\1}', dcfg, flags=re_flags)
+    dcfg = dcfg.replace("ansible_host: '{{public_ip}}'",
+                        "ansible_host: \"{{public_ip}}\"")
     if out_file:
         with open(out_file, 'w') as fic:
             fic.write(dcfg)
@@ -171,4 +185,82 @@ def export_makinastates(export_dir='/tmp', to_file=False):
         r = export_compute_node(id_, out_file=out, lxc=False)
         ret[id_] = r
     return ret
+
+
+EXPORT_CMD = '''
+ANSIBLE_FORCE_COLOR= ANSIBLE_NOCOLOR=1 ANSIBLE_STDOUT_CALLBACK=json \
+    $COPS_ROOT/bin/ansible-playbook -i {inv} -l localhost \
+        $COPS_ROOT/roles/corpusops.roles/export/role.yml'''
+
+
+class AnsibleError(Exception):
+    pass
+
+
+def get_ansible_inventory(item=None,
+                          inv='/etc/ansible/inventory',
+                          mode=None):
+    _s = __salt__  # noqa
+    export_mode = {
+        'host': 'export_host',
+        'group': 'export_group',
+    }.get(mode, '')
+    cmd = EXPORT_CMD.format(inv=inv)
+    if item and export_mode:
+        cmd += " -e {0}={1}".format(export_mode, item)
+    ret = _s['cmd.run_all'](cmd, python_shell=True)
+    if ret['retcode'] != 0:
+        print(cmd)
+        print(ret['stdout'])
+        print(ret['stderr'])
+        raise AnsibleError('invalid ansible return')
+    aret = _s['mc_dumper.json_load'](ret['stdout'])
+    rets = aret['plays'][0]['tasks'][-1]['hosts']
+    val = rets[[a for a in rets][0]]
+    if not isinstance(val['msg'], dict):
+        raise AnsibleError('invalid ansible return type')
+    for k in 'groups', 'vars':
+        val['msg']['d{0}'.format(k)] = val['msg'][k]
+        if isinstance(val['msg'][k], dict):
+            continue
+        val['msg']['d{0}'.format(k)] = _s['mc_dumper.json_load'](val['msg'][k])
+    return val['msg']
+
+
+def refresh_backup_monit(groups="backup",
+                         inv='/etc/ansible/inventory',
+                         burp_server_group_key='burp_server_group',
+                         to_file=None):
+    _s = __salt__  # noqa
+    lgroups = groups.split(',')
+    arets = {}
+    arets['full'] = get_ansible_inventory(inv=inv)
+    for g in lgroups:
+        arets[g] = get_ansible_inventory(g, inv=inv, mode='group')
+    ret = {}
+    conf = _s['mc_pillar.query'](
+        'supervision_configurations', {}
+    ).get('definitions', {}).get('autoconfigured_hosts', {})
+    for g in lgroups:
+        backuped_hosts = sorted([a for a in arets[g]['dgroups'][g]])
+        gserver = arets[g]['vars'][burp_server_group_key]
+        bserver = arets['full']['dgroups'][gserver][0]
+        for h in backuped_hosts:
+            monit = conf.setdefault(h, OrderedDict())
+            sattrs = monit.setdefault('services_attrs', OrderedDict())
+            ba = sattrs.setdefault('backup_burp_age', OrderedDict())
+            ba.setdefault('vars.ssh_port', 22)
+            ba.setdefault('vars.data_dir', '/data/burp2')
+            ba.setdefault('vars.ssh_host', bserver)
+    if to_file:
+        econf = {
+            'supervision_configurations': {
+                'definitions': {
+                    'autoconfigured_hosts': conf}}}
+        dcfg = _s['mc_dumper.yaml_dump'](econf)
+        with open(to_file, 'w') as fic:
+            fic.write(dcfg)
+            print('exported: {0}'.format(to_file))
+    return conf
+
 # vim:set et sts=4 ts=4 tw=80:
