15ccdb83dd7238f4632c4474e656e806d4905032
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-11-04 16:26:24 +0100

    fix slapdle cron

--- files/etc/slapd_le.sh
+++ files/etc/slapd_le.sh
@@ -5,6 +5,10 @@ rsync -azv /home/certbot/mastersalt/live/ /etc/ssl/le/
 chown -Rf openldap:openldap /etc/ssl/le/
 if [[ -z ${NO_RECONFIGURE-} ]];then
     /srv/makina-states/bin/salt-call --retcode-passthrough -lall state.sls makina-states.services.dns.slapd
-    systemctl restart slapd
+    if ( which systemctl &>/dev/null );then
+        systemctl restart slapd
+    else
+        service slapd restart
+    fi
 fi
 # vim:set et sts=4 ts=4 tw=80:
